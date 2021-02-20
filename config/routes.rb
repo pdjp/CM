e89a8979565af40f1cde32ea2320bc738e07ca75
pdjp <kknntt0522@gmail.com>
2021-02-20 21:11:33 +0900

    master

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  post "logout" => "users#logout"
+  
   get 'shoes/index' => 'shoes#index'
   get 'shoes/new' => 'shoes#new' 
   get 'shoes/:id' => 'shoes#show'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_20_093507) do
+ActiveRecord::Schema.define(version: 2021_02_20_114812) do
 
   create_table "jackets", force: :cascade do |t|
     t.text "content"
@@ -55,6 +55,14 @@ ActiveRecord::Schema.define(version: 2021_02_20_093507) do
     t.string "color"
     t.string "size"
     t.string "image"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "password"
   end
 
 end
